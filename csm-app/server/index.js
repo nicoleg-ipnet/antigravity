a908d834c3b77576cf664518f612644a91b584e9
Nicole <nicole@csm.com>
2026-03-04 14:21:46 -0300

    Fix path-to-regexp wildcard bug for SPA routing

--- csm-app/server/index.js
+++ csm-app/server/index.js
@@ -196,7 +196,7 @@ const path = require('path');
 const clientDistPath = path.join(__dirname, '../client/dist');
 app.use(express.static(clientDistPath));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(clientDistPath, 'index.html'));
 });
 
